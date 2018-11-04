02381850925dcbdc85a6a08357700fa5987fcfb2
ponponok <59070017@it.kmitl.ac.th>
2018-11-05 03:08:17 +0700

    CAL DATE TIME

--- app/src/main/java/healthypond017/healthy/sleep/SleepAdapter.java
+++ app/src/main/java/healthypond017/healthy/sleep/SleepAdapter.java
@@ -9,7 +9,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import healthypond017.healthy.R;
 
@@ -32,8 +35,30 @@ public class SleepAdapter extends ArrayAdapter<Sleep> {
         _wake = v.findViewById(R.id.sleep_item_wake);
         Sleep row = this._sleep.get(position);
         _date.setText(row.getDate());
-        _sleep.setText(row.getSleep());
-        _wake.setText(row.getWake());
+        _sleep.setText(row.getSleep()+" - "+row.getWake());
+        _wake.setText(timeCompare(row.getSleep(), row.getWake()));
         return v;
+    }
+
+    String timeCompare(String t1, String t2){
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+        String dateStart = "14 "+t1;
+        String dateStop = "15 "+t2;
+        SimpleDateFormat format2 = new SimpleDateFormat("dd HH:mm");
+        try {
+            if(format.parse(t2).getTime() > format.parse(t1).getTime()){
+                dateStop ="14 "+t2;
+            }
+            Date d1 = format2.parse(dateStart);
+            Date d2 = format2.parse(dateStop);
+            long diff = d2.getTime() - d1.getTime();
+            long hours = TimeUnit.MILLISECONDS.toHours(diff);
+            long minutes = TimeUnit.MILLISECONDS.toMinutes(diff) % 60;
+            return String.format("%02d:%02d", hours, minutes);
+        }catch (Exception e){
+            e.printStackTrace();
+            return "";
+        }
+
     }
 }
